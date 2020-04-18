bbde916f0e6b94d207b9f2312fc3539bc297edf6
Kasia Wrzesniak <kasia.wrzesniak@trivago.com>
2020-04-18 18:31:24 +0200

    Shitty comments blocked by CORS

--- src/main.js
+++ src/main.js
@@ -2,9 +2,50 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+// const dictionaryAPIurl =
+// "https://s3-eu-west-1.amazonaws.com/hungarian.phrasebook/example_get_entities.json";
+// const requestInitData = {
+//   method: "GET",
+//   mode: "no-cors"
+//   // credentials: "include"
+// };
+// const request = new Request(dictionaryAPIurl, requestInitData);
+// const requestOptions = { credentials: "omit" };
+
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    // TO DO: move and import this fn
+    // fetch(dictionaryAPIurl, {
+    //   method: "GET",
+    //   mode: "no-cors",
+    //   headers: {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Content-Type": "application/json"
+    // }
+    // fetch(request)
+    // .then(data => {
+    //   // eslint-disable-next-line
+    //   debugger;
+    //   console.log(data.JSON());
+    //   return data;
+    // })
+    // .then(response => {
+    //   debugger;
+    //   return response.json().then(parsedJSON => {
+    //     parsedJSON
+    //       ? console.log("parsed ", parsedJSON)
+    //       : console.log(" ampty");
+    //     return parsedJSON;
+    //   });
+    // })
+    // .then(response => (this.api = response))
+    // .catch(err => console.log("err: ", err));
+    // axios
+    // .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    // .then(response => (this.info = response));
+  }
 }).$mount("#app");
