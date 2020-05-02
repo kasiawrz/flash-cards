169d5e33bd53e41508f2caa4098bd3fce544c354
Kasia Wrzesniak <kasia.wrzesniak@trivago.com>
2020-05-02 22:07:10 +0200

    Flip the card

--- src/components/card-page/CardPage.vue
+++ src/components/card-page/CardPage.vue
@@ -1,47 +1,62 @@
 <template>
   <div>
     <h2>{{ msg }}</h2>
-    <div>CARDS</div>
+    <div>CARDS {{ cards }}</div>
     <!-- <SingleCard v-for="entity in formatedCards" :key="entity.id" /> -->
-    <SingleCard />
+    <SingleCard v-bind:cards="cards[0]" />
   </div>
 </template>
 
 <script>
 import SingleCard from "./SingleCard.vue";
-import * as APIresponse from "../../assets/APIresponse.json";
+import * as APIresponse from "../../utils/APIresponse.json";
 
 export default {
   name: "CardPage",
   components: { SingleCard },
   data: () => {
-    const languageNative = "en";
-    const languageToLearn = "es";
-    const cards = APIresponse.default;
-    // TO DO: filter by content type
-    const getFrontAndBackCard = card => {
-      let frontCard = null;
-      let backCard = null;
-
-      card.representations.forEach(repr => {
-        if (repr.type !== "text") return;
-
-        if (repr.language.indexOf(languageNative) === 0) {
-          frontCard = repr.content;
-        } else if (repr.language.indexOf(languageToLearn) === 0) {
-          backCard = repr.content;
-        }
-      });
-
-      return frontCard && backCard && { frontCard, backCard };
-    };
-
-    const formatedCards = cards.map(getFrontAndBackCard);
-    console.log("debug📍: CardPage 18", formatedCards);
-    return { cards: formatedCards };
+    return { cards: [] };
   },
   props: {
     msg: String
+  },
+  created: function() {
+    this.fetchData();
+  },
+  computed: {
+    currentCard() {
+      return this.cards[this.currentCardIndex];
+    }
+  },
+  methods: {
+    fetchData: function() {
+      const languageNative = "en";
+      const languageToLearn = "es";
+      const cards = APIresponse.default;
+
+      const getFrontAndBackCard = card => {
+        let frontCard = null;
+        let backCard = null;
+
+        card.representations.forEach(repr => {
+          // TO DO: filter by content type
+          if (repr.type !== "text") return;
+
+          if (repr.language.indexOf(languageNative) === 0) {
+            frontCard = repr.content;
+          } else if (repr.language.indexOf(languageToLearn) === 0) {
+            backCard = repr.content;
+          }
+        });
+
+        return frontCard && backCard && { frontCard, backCard };
+      };
+
+      const formatedCards = cards.map(getFrontAndBackCard);
+      console.log("debug📍: CardPage 18", formatedCards);
+
+      this.cards = formatedCards;
+    }
   }
 };
 </script>
--- src/components/card-page/SingleCard.vue
+++ src/components/card-page/SingleCard.vue
@@ -1,34 +1,53 @@
 <template>
   <div class="maincontainer">
-    <sui-card class="raised centered card">
-      <sui-card-content class="card__front orange">
+    <sui-card
+      class="raised centered card"
+      v-bind:class="{ isFlipped: isFlipped }"
+    >
+      <sui-card-content class="card__front orange" @click="flipCard">
         <sui-card-header>Cute Dog</sui-card-header>
         <h2>Front of Card</h2>
         <p>Front...</p>
+        <img
+          src="https://img.icons8.com/cute-clipart/64/000000/reply-arrow.png"
+        />
       </sui-card-content>
       <sui-card-content extra class="card__back blue">
         <sui-card-header>Translation</sui-card-header>
         <p>Translation...</p>
-        <sui-button basic positive @click="handleCorrectAnswer"
-          >Correct</sui-button
+        <sui-button basic positive @click="handleAnswer(1)"
+          >Correct 👍🏻</sui-button
         >
-        <sui-button basic negative>Wrong</sui-button>
+        <sui-button basic negative @click="handleAnswer(0)"
+          >Wrong 🛑 {{ counter }}</sui-button
+        >
       </sui-card-content>
     </sui-card>
   </div>
 </template>
 
 <script>
-export default {
+// export default {
+module.exports = {
   name: "SingleCard",
   props: {
-    // word: String,
-    // translation: String
-    entity: Object
+    currentCard: { type: Object }, //required: true },
+    counter: Number
   },
+  data: () => {
+    return {
+      isFlipped: false
+    };
+  },
+  computed: {},
   methods: {
-    handleCorrectAnswer: () => {
-      console.log("debug📍: SingleCard 33");
+    flipCard: function() {
+      this.isFlipped = this.isFlipped ? false : true;
+      console.log("debug📍: SingleCard 46", this.isFlipped);
+    },
+    handleAnswer: function(points) {
+      this.flipCard();
+      console.log("debug📍: SingleCard 33", points);
     }
   }
 };
